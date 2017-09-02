8c8a6b58c6821da2a12425c68f9d67bc9b47b5fc
lavanya0398 <lavanyajayachith@gmail.com>
2017-09-02 20:09:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool=require('pg').Pool;
 var config={
     user:'lavanyajayachith',
     database:'lavanyajayachith',
-    host:'db.imad.hasura-app.io',
+    host:'localhost',
     port:'5432',
     password:process.env.DB_PASSWORD
 };
